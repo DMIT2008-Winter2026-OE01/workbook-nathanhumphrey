9b096abcb3d0593286bdb1a1e25a3801772a5efe
Nathan Humphrey <nathpaulhump@gmail.com>
2026-01-12 18:57:42 -0700

    Update Lesson 02 - comments

--- src/lesson-02-break-activities/js/index.js
+++ src/lesson-02-break-activities/js/index.js
@@ -5,9 +5,16 @@ HTML for list topic list item
 </li>
 */
 
+// Use `const` since these variables are not expected to change
+// Access elements in the DOM using the `querySelector` function
 const topicForm = document.querySelector('.new-topic-form');
 const topicsList = document.querySelector('.topics-list');
 
+/**
+ * Adds a new topic to a list.
+ * @param {string} topicName - the new topic to add
+ * @param {HTMLElement} topicsListElement - the existing list to add to
+ */
 function addTopicToPage(topicName, topicsListElement) {
   const newTopicItem = `<li class="list-group-item">
       ${topicName}
@@ -19,13 +26,16 @@ topicForm.addEventListener('submit', (event) => {
   // Do not submit the form and reload the page!
   event.preventDefault();
 
+  // Capture both the input element and its value
   const input = topicForm.elements['new-topic'];
   const inputValue = input.value;
 
+  // Using Bootstrap's ServerSide validation classes for UI updates
   if (inputValue === '') {
     input.classList.add('is-invalid');
   } else {
     input.classList.remove('is-invalid');
+    // Input is good, add it to the page
     addTopicToPage(inputValue, topicsList);
   }
 });
