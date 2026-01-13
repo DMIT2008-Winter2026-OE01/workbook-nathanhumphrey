ef29e72b23b58c8d4538bd3ac77fa6e3f2cd7d14
Nathan Humphrey <nathpaulhump@gmail.com>
2026-01-12 18:47:26 -0700

    Update Lesson 02 - form submit and validation

--- src/lesson-02-break-activities/js/index.js
+++ src/lesson-02-break-activities/js/index.js
@@ -7,3 +7,17 @@ HTML for list topic list item
 
 const topicForm = document.querySelector('.new-topic-form');
 const topicsList = document.querySelector('.topics-list');
+
+topicForm.addEventListener('submit', (event) => {
+  // Do not submit the form and reload the page!
+  event.preventDefault();
+
+  const input = topicForm.elements['new-topic'];
+  const inputValue = input.value;
+
+  if (inputValue === '') {
+    input.classList.add('is-invalid');
+  } else {
+    input.classList.remove('is-invalid');
+  }
+});
