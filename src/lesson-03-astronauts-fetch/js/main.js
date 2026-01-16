0ed370a8eb2734b5572f74fe6153254f69501a0c
Nathan Humphrey <nathpaulhump@gmail.com>
2026-01-15 18:43:59 -0700

    Lesson 03 - export and import fetch

--- src/lesson-03-astronauts-fetch/js/main.js
+++ src/lesson-03-astronauts-fetch/js/main.js
@@ -1,1 +1,10 @@
 // main.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { getAstronautList } from './api/astronaut.js';
+
+async function app() {
+  let astronauts = await getAstronautList();
+  console.log(astronauts);
+}
+
+app();
