405ede43e0d286c6abebec139d5a00c97e7b8e3d
Nathan Humphrey <nathpaulhump@gmail.com>
2026-01-19 16:47:06 -0700

    Lesson 03 - Add comments

--- src/lesson-03-astronauts-fetch/js/api/astronaut.js
+++ src/lesson-03-astronauts-fetch/js/api/astronaut.js
@@ -5,9 +5,13 @@ const BASE_URL = "https://lldev.thespacedevs.com/2.3.0";
 // OR, use an arrow function
 const getAstronautList = async () => {
   const endpoint = `${BASE_URL}/astronauts?mode=list&nationality=Canadian`;
+  // First, capture the response object
   const res = await fetch(endpoint);
+  // Then, extract the JSON from the response object
   const json = await res.json();
+  // Finally, return the JSON data
   return json;
 }
 
+// Export functions here
 export { getAstronautList };
--- src/lesson-03-astronauts-fetch/js/main.js
+++ src/lesson-03-astronauts-fetch/js/main.js
@@ -1,10 +1,17 @@
 // main.js
+
+// Import CSS files, Vite will bundle them automatically
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Import custom JS modules
 import { getAstronautList } from './api/astronaut.js';
 
+// Wrap the code in an async function to use top-level
+// await (not necessary since ES2022 [13th version], but 
+// kept here for compatibility)
 async function app() {
   let astronauts = await getAstronautList();
   console.log(astronauts);
 }
 
+// Run the application
 app();
