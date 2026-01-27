b876374f4e29afac2304896d9e3c896fb66795e0
Nathan Humphrey <NathanH@nait.ca>
2026-01-27 09:18:52 -0700

    Add Lesson 5 starter

---
+++ src/lesson-05-react-components/pages/index.js
@@ -0,0 +1,30 @@
+export default function Home() {
+  return (
+    <div className="max-w-[1200px] mx-auto p-4">
+      <h1 className="text-2xl font-bold">Lesson 05 - React Components</h1>
+      <hr className="border-2 border-sky-600 mt-2 mb-4" />
+      <form>
+        <div className="mb-3">
+          <div class="flex items-center space-x-4">
+            <div class="flex flex-col mb-4">
+              <label htmlFor="new-topic-input" className="text-md font-bold">Activity description</label>
+              <input type="text" name="new-topic" className="p-1.5 border border-solid border-sky-600 rounded-md focus-visible:outline-sky-700 focus-visible:outline-2" id="new-topic-input" />
+            </div>
+            <button type="submit" className="mt-2 p-1.5 border border-sky-600 rounded-md bg-sky-600 font-bold text-white hover:bg-sky-700 active:bg-sky-900 focus-visible:outline-sky-700 focus-visible:outline-2">
+              Add Actvitiy
+            </button>
+          </div>
+          <div className="text-red-600 mt-1 hidden">
+            Please enter a valid activity
+          </div>
+        </div>
+      </form>
+      <h2 className="mt-2 text-xl font-bold">Activities</h2>
+      <ul className="topics-list">
+        <li className="my-1 pl-1 border-b border-t border-sky-600">Actitivity 1</li>
+        <li className="my-1 pl-1 border-b border-sky-600">Actitivity 2</li>
+        <li className="my-1 pl-1 border-b border-sky-600">Actitivity 3</li>
+      </ul>
+    </div>
+  );
+}
